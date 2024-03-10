285332b1e6e15449f079d08e4e53313d51a43021
RadwanAlboom <51923169+RadwanAlboom@users.noreply.github.com>
2024-03-10 17:27:59 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 // create the pool
 const pool = mysql.createPool({
-    host: 'eu-cdbr-west-01.cleardb.com',
-    user: 'bc35cbfa307ff4',
-    password: '5954f772',
-    database: 'heroku_b4b2aa704af1467',
+    host: 'student-database.cpcu66omk4jt.eu-central-1.rds.amazonaws.com',
+    user: 'radwan',
+    password: 'aboalrood123$',
+    database: 'app_student_db',
 });
 // now get a Promise wrapped instance of that pool
 const promisePool = pool.promise();
